e0fa3110fa9a3073cfeff6512647eac8590d770f
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-12-08 23:31:10 +0330

    update

--- src/components/Loginform/Login.jsx
+++ src/components/Loginform/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
 const Login = () => {
   return (
-    <div>Welcome to Login Page</div>
-  )
-}
- 
-export default Login
+    <div className="wrapper ">
+      <form action="text">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="username " required />
+        </div>
+        <div className="input-box">
+          <input type="password " placeholder="Password " required />
+        </div>
+        <div className="remember-forgot">
+          <label htmlFor="">
+            <input type="checkbox" /> Rememeber me
+          </label>
+          <a href="#">Forgot password?</a>
+        </div>
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>
+            Don't have an account
+            <a href="#">Register</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
